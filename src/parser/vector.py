5e2fc06f99337e110c70b969ea9bcb58010ef55a
artem abernikhin <artem.abernikhin@gmail.com>
2025-10-26 18:47:44 +0200

    token vector

--- src/parser/vector.py
+++ src/parser/vector.py
@@ -1,5 +1,46 @@
+import lexer
+
 class vector:
-    def __init__(self, _values):
+    def __init__(self, _values, type):
         if type(_values) != list:
             raise TypeError("wrong type in vector")
-        self.v = _values
+        for i in _values:
+            if type(i) != lexer.token or type(i) != vector:
+                raise TypeError("wrong type elements in list")
+        self.type = type
+        self.v = self.parent(_values)
+
+    def append(self, obj):
+        if type(obj) != lexer.token or type(obj) != vector:
+            raise TypeError("wrong type in vector.append")
+        self.v.append(obj)
+    
+    def parent(self, tokens):
+        result = []
+        buf    = vector([], 0)
+        index  = 0
+        c      = 0
+        while index < len(tokens):
+            i = tokens[index]
+            if i == '{':
+                if c != 0:
+                    buf.append(i)
+                c += 1
+                index += 1
+                continue
+            if i == '}':
+                c -= 1
+                index += 1
+                if c != 0:
+                    buf.append(i)
+                if c == 0:
+                    result.append(buf)
+                    buf = vector([], 0)
+                continue
+            if c != 0:
+                buf.append(i)
+                index += 1
+                continue
+            result.append(i)
+            index += 1
+        return result
